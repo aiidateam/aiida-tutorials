a938d7d8613dbad60855c18875c5b548520cf3bf
Chris Sewell <chrisj_sewell@hotmail.com>
2020-07-05 09:46:09 +0100

    fixes

--- docs/conf.py
+++ docs/conf.py
@@ -29,11 +29,8 @@ extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
     'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_console_highlighting',
-    'IPython.sphinxext.ipython_directive',
-    'myst_nb',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.mathjax',
-    'sphinx_copybutton',
+    'IPython.sphinxext.ipython_directive', 'myst_nb', 'sphinx.ext.extlinks',
+    'sphinx.ext.mathjax', 'sphinx_copybutton', 'sphinx_panels'
 ]
 ipython_mplbackend = ""
 
